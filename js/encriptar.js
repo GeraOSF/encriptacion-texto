e631ef5457207385a2dabda8f1d0a0dcf160df54
Gerardo Gómez <gerardogomezromero175@gmail.com>
2022-04-20 19:39:03 -0700

    La funcion de encriptacion completada

--- js/encriptar.js
+++ js/encriptar.js
@@ -1,6 +1,33 @@
+// Declaracion de constante para el boton y de las llaves
 const botonEncriptar = document.querySelector("#boton-encriptar");
+const llaves = {
+    "a": "ai",
+    "e": "enter",
+    "i": "imes",
+    "o": "ober",
+    "u": "ufat"
+};
 
+// Event listener para el boton de encriptar al hacer click en el
 botonEncriptar.addEventListener("click", () => {
-    const textIn = document.querySelector(".text-in");
+    // Declaracion de constantes del texto de entrada y su tamaño
+    const textIn = document.querySelector(".text-in").value;
+    const len = textIn.length;
     
-})
+    // Iterando por cada caracter del texto entrada
+    let textoBuffer = "";
+    for (let i = 0; i < len; i++) {
+        // Si el caracter esta en llaves, concatenar el valor de esa llave en textoBuffer
+        if (textIn[i] in llaves) {
+            textoBuffer += llaves[textIn[i]];
+        }
+        // Si no esta en llaves, concatenar el caracter sin modificarlo 
+        else {
+            textoBuffer += textIn[i];
+        }
+    }
+    
+    // Poner el texto codificado a textOut (p)
+    const textOut = document.querySelector(".text-out");
+    textOut.textContent = textoBuffer;
+});
